66e0711a96712d5824a739b422c53d57a1ee55eb
CJC <jiecchen@indiana.edu>
2014-08-07 14:57:39 -0400

    add comments

--- src/BJKST.py
+++ src/BJKST.py
@@ -44,7 +44,37 @@ class _BJKST_Estimator:
 
 
 class BJKST:
+    """
+    BJKST sketch for estimation the distinct frequency.
+    Algorithm and Analysis can be found in:
+    http://www.cs.dartmouth.edu/~ac/Teach/CS49-Fall11/Notes/lecnotes.pdf
+    or 
+    https://github.com/jiecchen/references/blob/master/lecnotes.pdf
+
+    Usage:
+    @args
+    n    :  the size of universe
+    eps, delta: control the quality of estimation
+    @return
+    BJKST(n, eps, delta) returns  an (eps, delta) - BJKST sketch with para eps and delta.
+
+    Example:
+    ------------------
+    d = DataStream(list("qwertyuiopasdfghjklzxcvbnm"), 1000)
+    sketch = BJKST(26, 0.1, 0.001)
+    for x in d:
+        sketch.process(x)
+    print sketch.getEstimation()
+    ------------------
+    """
     def __init__(self, n, eps, delta = 0.01):
+        """
+        @args
+        n    :  the size of universe
+        eps, delta: control the quality of estimation
+        @return
+        BJKST(n, eps, delta) returns  an (eps, delta) - BJKST sketch with para eps and delta.
+        """
         uhash_h = UniversalHash(n)
         uhash_g = UniversalHash(int(math.log(n, 2)**2 * eps**(-4)))
         thresh = eps**(-2)
@@ -53,10 +83,12 @@ class BJKST:
         self.estimators = [_BJKST_Estimator(eps, thresh, uhash_h, uhash_g) for i in range(n_hash)]
     
     def process(self, key):
+        """ process the key """
         for est in self.estimators:
             est.process(key)
             
     def getEstimation(self):
+        """ return the (eps, delta)-approximation """
         return median( [est.getEstimation() for est in self.estimators] )
 
 
