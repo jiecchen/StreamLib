49b8ebeee33dc5bb2cec7eaae86075e21cf93ed4
CJC <jiecchen@indiana.edu>
2014-08-08 01:22:34 -0400

    add countSketch

---
+++ streamlib/sketch/countSketch.py
@@ -0,0 +1,51 @@
+from streamlib.hashes.universalHashing import UniversalHash
+from streamlib.utils import zeros, median
+import math
+import random
+class _CountSketch_estimator:
+    """ Basic estimator for Count Sketch """
+    def __init__(self, k, uhash_h, uhash_g):
+        """  
+        @args
+        eps      :  control accuracy
+        uhash_h  : an instance of random.Random
+        """
+        self.k = k
+        self.C = [0 for i in range(self.k)]
+        self.h = uhash_h.pickHash()
+        self.g = uhash_g.pickHash()
+        
+    def process(self, key):
+        self.C[self.h.hash(key)] +=  1 - 2 * self.g.hash(key)
+
+    def getEstimation(self, key):
+        return (1 - 2 * self.g.hash(key)) * self.C[self.h.hash(key)]
+    
+    
+        
+        
+
+
+class CountSketch:
+    def __init__(self, eps, delta = 0.01):
+        """
+        @args
+        @return
+        """
+        k = 3. * eps**(-2)
+        # make sure self.k in the form 2^m
+        self.k = 1 << (int(math.log(k, 2)) + 1)
+        uhash_h = UniversalHash(self.k)
+        uhash_g = UniversalHash(2)
+        n_hash = int(math.log(1. / delta, 2)) + 1
+        self.estimators = [_CountSketch_estimator(self.k, uhash_h, uhash_g) for i in range(n_hash)]
+
+    def process(self, key):
+        """ process the key """
+        for est in self.estimators:
+            est.process(key)
+        
+    def getEstimation(self, key):
+        """ return the (eps, delta)-approximation """
+        return median( [est.getEstimation(key) for est in self.estimators] )
+
