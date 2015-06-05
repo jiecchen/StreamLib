7e41e4d7e9a541dcdd613ba993bd0f3a3836df55
CJC <jiecchen@indiana.edu>
2015-06-05 05:38:16 -0400

    updated

--- setup.py
+++ setup.py
@@ -2,15 +2,27 @@
 from setuptools import setup, find_packages
 from codecs import open
 import os
-
+from distutils.core import setup, Command
 
 def read(fname):
     path = os.path.join(os.path.dirname(__file__), fname)
     return open(path, encoding='utf-8').read()
 
+# class PyTest(Command):
+#     user_options = []
+#     def initialize_options(self):
+#         pass
+#     def finalize_options(self):
+#         pass
+#     def run(self):
+#         import sys,subprocess
+#         errno = subprocess.call([sys.executable, 'runtests.py'])
+#         raise SystemExit(errno)
+
+
 
 setup(
-    name="streamlib",
+    name="StreamLib",
     version="1.0.1",
     packages=find_packages(),
 
@@ -20,30 +32,27 @@ setup(
     # metadata for upload to PyPI
     author="Jiecao Chen",
     author_email="chenjiecao@gmail.com",
-    description="TinyDB is a tiny, document oriented database optimized for "
-                "your happiness :)",
+    description="StreamLib: Library of streaming algorithms ",
     license="MIT",
-    keywords="streaming algorithms",
+    keywords="Streaming Algorithms",
     url="https://github.com/jiecchen/StreamLib",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Researchers",
-        "Intended Audience :: Data Scientists",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
         "License :: OSI Approved :: MIT License",
-        "Topic :: Algorithms",
-        "Topic :: Algorithms :: Streaming Algorithms",
+        "Topic :: Database",
+        "Topic :: Database :: Database Engines/Servers",
         "Topic :: Utilities",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
 #        "Programming Language :: Python :: 3.3",
-#        "Programming Language :: Python :: 3.4",
-#        "Programming Language :: Python :: Implementation :: PyPy",
+ #       "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: Implementation :: PyPy",
         "Operating System :: OS Independent"
     ],
 
     long_description=read('README.md'),
-    install_requires=[
-        'mmh3',
-    ],
+    install_requires=['mmh3'],
+#    cmdclass = {'test': PyTest},
 )
-
