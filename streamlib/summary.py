a0af9c1143c0ed518ad298783998ba4b1b7500e0
CJC <jiecchen@indiana.edu>
2015-06-05 16:03:20 -0400

    add merge

--- streamlib/summary.py
+++ streamlib/summary.py
@@ -2,12 +2,14 @@
 
 """
 from streamlib import MurmurHash
+import copy
+from array import array
 
 class CountMin:
     """
     Count-Min sketch.
     """
-    def __init__(self, w=20, mu=5):
+    def __init__(self, w=20, mu=5, typecode='i'):
         """
         Create a new instance of CountMin.
 
@@ -17,11 +19,15 @@ class CountMin:
         :param mu: The number of repeated copies. Used to control the
                    failure probability ~= 2^{-mu}
         :type mu: int
+
+        :param typecode: type to represent the frequencies, check
+                         docs.python.org for module `array`
         """
         self._w = w
         self._mu = mu
-        self._sketch = [[0] * w for i in xrange(mu)]
+        self._sketch = [array(typecode, [0] * w) for i in xrange(mu)]
         self._hashes = [MurmurHash() for i in xrange(mu)]
+        self._hash = hash(self) 
 
     def processBatch(self, dataStream, weighted=False):
         """
@@ -54,7 +60,7 @@ class CountMin:
 
         :param item: hashable object to be processed
                            e.g. an integer
-        :param weighted: if weighted, each item  should
+        :param weighted: if weighted, item  should
                          be a (key, weight) pair, where weight > 0
         """
         if weighted:
@@ -86,4 +92,56 @@ class CountMin:
                           for i in xrange(self._mu)]
         return min(all_estimators)
 
+
+
+    def reproduce(self, num=1):
+        """
+        Reproduce CountMin instance(s) to have the same
+        internal status.
+
+        :param num: number of instances to be reproduced
+        :type num: int
+
+        :return: reproduced instance. if num > 1, a list 
+                 of instances will be returned
+        """
+        if type(num) is not int:
+            raise TypeError('num should be int')
+        if num < 1:
+            raise ValueError('num should >= 1')
+
+        if num == 1:
+            return copy.deepcopy(self)
+        else:
+            return [copy.deepcopy(self)]
+            
+        
+
+
+    def merge(self, other):
+        """
+        Merge two CountMin instances if they are compatible.
+
+        :param other: an instance of CountMin, 
+        """
+        if other._hash != self._hash:
+            raise ValueError('two instances are not compatible')
+
+        res = CountMin(w=1, mu=1)
+        res._sketch = copy.deepcopy(self._sketch)
+        res._hashes = copy.deepcopy(self._hashes)
+        res._w = self._w
+        res._mu = self._mu
+        for i in xrange(self._mu):
+            for j in xrange(self._w):
+                res._sketch[i][j] += other._sketch[i][j]
+
+        return res
+
+                
+    def __add__(self, other):
+        """
+        Overload + for self.merge
+        """
+        return self.merge(other)
 
