5421a22db1b919ca90487ecdebbf0f4fcdc4faae
CJC <jiecchen@indiana.edu>
2015-06-05 00:33:46 -0400

    rm unused code

--- streamlib/wrappers.py
+++
@@ -1,9 +0,0 @@
-from inspect import getmembers, isfunction
-
-def inherit_docs(cls):
-    for name, func in getmembers(cls, isfunction):
-        if func.__doc__: continue
-        for parent in cls.__mro__[1:]:
-            if hasattr(parent, name):
-                func.__doc__ = getattr(parent, name).__doc__
-    return cls
